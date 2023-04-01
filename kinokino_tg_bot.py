445e14c904c6db1383c2232c68ff075a78375a5a
Artem <posevin2011@gmail.com>
2023-04-01 12:45:35 +0300

    commit 2

--- kinokino_tg_bot.py
+++ kinokino_tg_bot.py
@@ -15,11 +15,13 @@ if __version_info__ < (20, 0, 0, "alpha", 1):
         f"{TG_VER} version of this example, "
         f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
     )
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram import ForceReply, InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import (
     Application,
     CallbackQueryHandler,
+    MessageHandler,
     CommandHandler,
+    filters,
     ContextTypes,
     ConversationHandler,
 )
@@ -33,107 +35,11 @@ logger = logging.getLogger(__name__)
 URL_KINOKINO = 'http://192.168.0.150:9200/'
 URL_USER = 'v1/user/'
 URL_START = 'v1/start/'
+URL_SEARCH_FILM = 'v1/search_film/'
+
 # START_ROUTES, END_ROUTES = range(2)
 # ONE, TWO, THREE, FOUR = range(4)
-
-
-# async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-#     user = update.message.from_user
-#     logger.info("User %s started the conversation.", user.first_name)
-#     keyboard = [
-#         [
-#             InlineKeyboardButton("1", callback_data=str(ONE)),
-#             InlineKeyboardButton("2", callback_data=str(TWO)),
-#         ]
-#     ]
-#     reply_markup = InlineKeyboardMarkup(keyboard)
-#     await update.message.reply_text("Start handler, Choose a route", reply_markup=reply_markup)
-#     return START_ROUTES
-
-
-# async def start_over(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-#     query = update.callback_query
-#     await query.answer()
-#     keyboard = [
-#         [
-#             InlineKeyboardButton("1", callback_data=str(ONE)),
-#             InlineKeyboardButton("2", callback_data=str(TWO)),
-#         ]
-#     ]
-#     reply_markup = InlineKeyboardMarkup(keyboard)
-#     await query.edit_message_text(text="Start handler, Choose a route", reply_markup=reply_markup)
-#     return START_ROUTES
-#
-#
-# async def one(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-#     query = update.callback_query
-#     await query.answer()
-#     keyboard = [
-#         [
-#             InlineKeyboardButton("3", callback_data=str(THREE)),
-#             InlineKeyboardButton("4", callback_data=str(FOUR)),
-#         ]
-#     ]
-#     reply_markup = InlineKeyboardMarkup(keyboard)
-#     await query.edit_message_text(
-#         text="First CallbackQueryHandler, Choose a route", reply_markup=reply_markup
-#     )
-#     return START_ROUTES
-#
-#
-# async def two(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-#     query = update.callback_query
-#     await query.answer()
-#     keyboard = [
-#         [
-#             InlineKeyboardButton("1", callback_data=str(ONE)),
-#             InlineKeyboardButton("3", callback_data=str(THREE)),
-#         ]
-#     ]
-#     reply_markup = InlineKeyboardMarkup(keyboard)
-#     await query.edit_message_text(
-#         text="Second CallbackQueryHandler, Choose a route", reply_markup=reply_markup
-#     )
-#     return START_ROUTES
-#
-#
-# async def three(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-#     query = update.callback_query
-#     await query.answer()
-#     keyboard = [
-#         [
-#             InlineKeyboardButton("Yes, let's do it again!", callback_data=str(ONE)),
-#             InlineKeyboardButton("Nah, I've had enough ...", callback_data=str(TWO)),
-#         ]
-#     ]
-#     reply_markup = InlineKeyboardMarkup(keyboard)
-#     await query.edit_message_text(
-#         text="Third CallbackQueryHandler. Do want to start over?", reply_markup=reply_markup
-#     )
-#     return END_ROUTES
-#
-#
-# async def four(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-#     query = update.callback_query
-#     await query.answer()
-#     keyboard = [
-#         [
-#             InlineKeyboardButton("2", callback_data=str(TWO)),
-#             InlineKeyboardButton("3", callback_data=str(THREE)),
-#         ]
-#     ]
-#     reply_markup = InlineKeyboardMarkup(keyboard)
-#     await query.edit_message_text(
-#         text="Fourth CallbackQueryHandler, Choose a route", reply_markup=reply_markup
-#     )
-#     return START_ROUTES
-#
-#
-# async def end(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-#     query = update.callback_query
-#     await query.answer()
-#     await query.edit_message_text(text="See you next time!")
-#     return ConversationHandler.END
+SEARCH, SEARCHING = range(2)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -141,14 +47,14 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     try:
         requests.get(URL_KINOKINO)
     except requests.ConnectionError:
-        await update.message.reply_text('Включи бота, лошара')
+        await update.message.reply_text('Включи сервер, лошара')
     json = {
         'username': f'{user.id}',
         'password': f'{user.id}',
     }
     response = requests.post(f'{URL_KINOKINO}{URL_START}',
                              json=json)
-    if response.status_code == 418:
+    if response.status_code == 200:
         await update.message.reply_text('🫖')
     if response.status_code == 201:
         await update.message.reply_text('Можно начинать (^˵◕ω◕˵^)')
@@ -159,36 +65,52 @@ async def my_id(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'{user.id}')
 
 
-async def my_profile(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def search(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     user = update.effective_user
+    await update.message.reply_text("Введите название... или /skip")
+    return SEARCHING
+
+
+async def searching(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    params = {'name': update.message.text}
+    searching_result = requests.get(f'{URL_KINOKINO}{URL_SEARCH_FILM}', params).json()
+    result_message = ''
+    for i, movie in enumerate(searching_result):
+        try:
+            movie_years = movie['releaseYears']
+            if movie_years:
+                movie_years = movie_years[0]
+                result_message += f"{i + 1}) {movie['name']} ({movie_years['start']}-{movie_years['end']})\n"
+        except KeyError:
+            result_message += f"{i+1}) {movie['name']} ({movie['year']})\n"
+    await update.message.reply_text(result_message)
+    return ConversationHandler.END
+
+
+async def selected_searching(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     pass
+
+
+async def skip(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    return ConversationHandler.END
 
 
 def main() -> None:
     application = Application.builder().token("5454514886:AAFL06SwdfSCkv_afMBIvT576G-sEE5_cvY").build()
 
-    # conv_handler = ConversationHandler(
-    #     entry_points=[CommandHandler("start", start)],
-    #     states={
-    #         START_ROUTES: [
-    #             CallbackQueryHandler(one, pattern="^" + str(ONE) + "$"),
-    #             CallbackQueryHandler(two, pattern="^" + str(TWO) + "$"),
-    #             CallbackQueryHandler(three, pattern="^" + str(THREE) + "$"),
-    #             CallbackQueryHandler(four, pattern="^" + str(FOUR) + "$"),
-    #         ],
-    #         END_ROUTES: [
-    #             CallbackQueryHandler(start_over, pattern="^" + str(ONE) + "$"),
-    #             CallbackQueryHandler(end, pattern="^" + str(TWO) + "$"),
-    #         ],
-    #     },
-    #     fallbacks=[CommandHandler("start", start)],
-    # )
-    #
-    # application.add_handler(conv_handler)
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("search", search)],
+        states={
+
+            SEARCHING: [MessageHandler(filters.TEXT & ~filters.COMMAND, searching), CommandHandler('skip', skip)]
+
+        },
+        fallbacks=[CommandHandler("skip", skip)],
+    )
+
+    application.add_handler(conv_handler)
 
     application.add_handler(CommandHandler('my_id', my_id))
-
-    application.add_handler(CommandHandler('my_profile', my_profile))
 
     application.add_handler(CommandHandler('start', start))
 
